7bc75fdfe8bc899610c0c94e53599cb8ddd279e9
aishah Ali <ayoshali13@gmail.com>
2025-05-12 12:48:19 +0300

    add delete method

--- main.js
+++ main.js
@@ -28,14 +28,17 @@
             let post = document.createElement("p");
             let img = document.createElement("img");
             let del = document.createElement("button");
+            let card = document.createElement("div");
+            card.classList.add("card");
             text.innerText=`name is: ${element.username}`
             post.innerText= element.textarea;
             img.src=element.img;
             del.innerText="delete"
-            userInfo.appendChild(text);
-            userInfo.appendChild(post);
-            userInfo.appendChild(img);
-            userInfo.appendChild(del);
+            card.appendChild(text);
+            card.appendChild(post);
+            card.appendChild(img);
+            card.appendChild(del);
+            userInfo.appendChild(card);
    
           del.addEventListener("click", ()=>{
           fetch(`https://68219a01259dad2655afc15b.mockapi.io/post/${element.id}`, {
