b5dbbf222a799b259fdd7ca49f8b3b48bbb2857f
aishah Ali <ayoshali13@gmail.com>
2025-05-12 12:38:47 +0300

    add delete method

---
+++ main.js
@@ -0,0 +1,47 @@
+     let btn =document.getElementById('btn');
+     let username = document.getElementById('username');
+     let textarea =document.getElementById('textarea');
+     let userInfo = document.getElementById('userInfo');
+     let img = document.getElementById('img');
+
+     btn.addEventListener("click", ()=>{
+        fetch('https://68219a01259dad2655afc15b.mockapi.io/post', {
+            method: 'POST',
+            body: JSON.stringify({
+                username: username.value,
+                textarea: textarea.value,
+                img: img.value
+            }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+     })
+     
+     fetch('https://68219a01259dad2655afc15b.mockapi.io/post')
+      .then(response => response.json())
+      .then((data) =>{
+        data.forEach(element => {
+            let text =document.createElement("h4");
+            let post = document.createElement("p");
+            let img = document.createElement("img");
+            let del = document.createElement("button");
+            text.innerText=`name is: ${element.username}`
+            post.innerText= element.textarea;
+            img.src=element.img;
+            del.innerText="delete"
+            userInfo.appendChild(text);
+            userInfo.appendChild(post);
+            userInfo.appendChild(img);
+            userInfo.appendChild(del);
+   
+          del.addEventListener("click", ()=>{
+          fetch(`https://68219a01259dad2655afc15b.mockapi.io/post/${element.id}`, {
+            method: 'DELETE',
+          })
+        }
+        )
+    })
+    })
